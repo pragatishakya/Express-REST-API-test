912dfb3af79737d73e965c077a323cc5ff922683
Pragati Shakya <pragatishakya12@gmail.com>
2023-02-13 09:33:59 +0530

    all get requests completed

--- index.js
+++ index.js
@@ -2,32 +2,35 @@ const express= require('express')
 const path=require('path')
 const app= express()
 
+//Array 
+const members=[{
+    id:1,
+    name:"John",
+    email:"john@john.com",
+    status:'active'
+},{
+    id:2,
+    name:"Steve",
+    email:"steve@steve.com",
+    status:'inactive'
+},{
+    id:3,
+    name:"mark",
+    email:"mark@mark.com",
+    status:'active'
+}]
 
-const middleware= (req,res,next)=>{
-    console.log("hi this is middleware")
-    next()
-}
-app.use(middleware)
-
-app.use(express.static(path.join(__dirname,'public')))
-
-//CRUD 
-app.get("/",middleware,(req,res)=>{                                  //READ
-    res.send("<h1>Hi this is GET Request</h1>")
+//To get all the users
+app.get("/showAllUser",(request, response)=>{
+    response.status(200).json(members)
 })
 
-app.post("/",(req,res)=>{                                 //CREATE
-    res.send("<h1>Hi this is POST Request</h1>")
+//To get the user who's id is mentioned in url
+app.get("/showUser/:uid",(request,response)=>{
+    const id=parseInt(request.params.uid)
+    const user= members.filter(member=>member.id === id)
+    user.length!==0 ? response.status(200).json(user) : response.status(200).json({message:"User not found"})
 })
-
-app.put("/",(req,res)=>{                                   //UPDATE
-    res.send("<h1>Hi this is PUT Request</h1>")
-})
-
-app.delete("/",(req,res)=>{                                 //DELETE
-    res.send("<h1>Hi this is DELETE Request</h1>")
-})
-
 
 const PORT=3000
 app.listen(PORT,()=>console.log(`Server is running ar ${PORT}`))
